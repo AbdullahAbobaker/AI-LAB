0eaac2f79cc33f17effccfaa8836187e7ab70432
Abobaker <binsalah8@gmail.com>
2025-04-17 17:15:17 +0300

    added cuda error tracker

--- UI/app.py
+++ UI/app.py
@@ -4,6 +4,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import streamlit as st
 from qa_engine import ask_question
+import torch
+if torch.cuda.is_available():
+    raise RuntimeError("CUDA is unexpectedly enabled!")
 
 st.set_page_config(page_title="🧠 Medizinischer Aufklärungs-Bot")
 
